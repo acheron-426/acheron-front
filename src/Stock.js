6421a49a1f55780e83fc294c7924178a823693c1
martypb <martypb+acheron@gmail.com>
2020-12-11 22:14:18 -0800

    added stock.js

---
+++ src/Stock.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+function StocksComponent() {
+  const [currentStock, setCurrentStock] = useState("");
+  const [stocks, setStocks] = useState([
+    {
+      todo: "GOR",
+      isCompleted: true
+    }
+  ]);
+
+  function createNewStock(currentStock) {
+    let stocksArray = [...stocks];
+    stocksArray.push({
+      stock: currentStock,
+      isCompleted: false
+    });
+    setStocks(stocksArray);
+  }
+
+  function completeStock(index) {
+    let stocksArray = [...stocks];
+    stockArray[index].isCompleted = !stocksArray[index].isCompleted;
+    setStocks(stocksArray);
+  }
+
+  function deleteStock(index) {
+    let stocksArray = [...stocks];
+    stocksArray.splice(index, 1);
+    setStocks(stocksArray);
+  }
+
+  return (
+    <div>
+      <input
+        className="stock-input"
+        value={currentStock}
+        onChange={e => {
+          setCurrentStock(e.target.value);
+        }}
+        onKeyPress={e => {
+          if (e.key === "Enter") {
+            createNewStock(currentStock);
+            setCurrentStock("");
+          }
+        }}
+        placeholder="Add ticker"
+      />
+      {stocks.map((stock, index) => (
+        <div key={stock} className="stock">
+          <div className="checkbox" onClick={() => completeStock(index)}>
+            {stock.isCompleted && <span>&#x2714;</span>}
+          </div>
+          <div className={stock.isCompleted ? "done" : ""}>{stock.stock}</div>
+          <div className="delete" onClick={() => deleteStock(index)}>
+            &#128465;
+          </div>
+        </div>
+      ))}
+      {stocks.length > 0 && `${stocks.length} stocks`}
+    </div>
+  );
+}
+export default StocksComponent;
