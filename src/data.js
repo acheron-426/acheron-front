275c2649191d6197177aaec98241dbe6df81d161
martypb <martypb+acheron@gmail.com>
2020-12-12 16:45:07 -0800

    added file stockData.js

---
+++ src/data.js
@@ -0,0 +1,90 @@
+export const stockData = {
+  "code": "GOR",
+  "isin_code": "AU000000GOR5",
+  "desc_full": "Ordinary Fully Paid",
+  "last_price": 1.225,
+  "open_price": 1.2,
+  "day_high_price": 1.23,
+  "day_low_price": 1.195,
+  "change_price": 0.025,
+  "change_in_percent": "2.083%",
+  "volume": 4316134,
+  "bid_price": 1.21,
+  "offer_price": 1.23,
+  "previous_close_price": 1.2,
+  "previous_day_percentage_change": "-2.439%",
+  "year_high_price": 2.02,
+  "last_trade_date": "2020-12-11T00:00:00+1100",
+  "year_high_date": "2020-07-23T00:00:00+1000",
+  "year_low_price": 0.805,
+  "year_low_date": "2020-03-16T00:00:00+1100",
+  "year_open_price": 0.175,
+  "year_open_date": "2014-02-25T11:00:00+1100",
+  "year_change_price": 1.05,
+  "year_change_in_percentage": "600%",
+  "pe": 30.02,
+  "eps": 0.0408,
+  "average_daily_volume": 6462471,
+  "annual_dividend_yield": 0,
+  "market_cap": 1055909698,
+  "number_of_shares": 879924748,
+  "deprecated_market_cap": 1077908000,
+  "deprecated_number_of_shares": 879924748,
+  "suspended": false,
+  "indices": [
+    {
+      "index_code": "XJO",
+      "name_full": "S&P/ASX 200",
+      "name_short": "S&P/ASX200",
+      "name_abrev": "S&P/ASX 200"
+    },
+    {
+      "index_code": "XKO",
+      "name_full": "S&P/ASX 300",
+      "name_short": "S&P/ASX300",
+      "name_abrev": "S&P/ASX 300"
+    },
+    {
+      "index_code": "XAO",
+      "name_full": "ALL ORDINARIES",
+      "name_short": "ALL ORDS",
+      "name_abrev": "All Ordinaries"
+    },
+    {
+      "index_code": "XSO",
+      "name_full": "S&P/ASX SMALL ORDINARIES",
+      "name_short": "Small Ords",
+      "name_abrev": "S&P/ASX Small Ords"
+    },
+    {
+      "index_code": "XAT",
+      "name_full": "S&P/ASX All Australian 200",
+      "name_short": "Aust 200",
+      "name_abrev": "All Australian 200"
+    },
+    {
+      "index_code": "XGD",
+      "name_full": "S&P/ASX All Ordinaries Gold (Sub Industry)",
+      "name_short": "S&P AO GLD",
+      "name_abrev": "Gold"
+    },
+    {
+      "index_code": "XMJ",
+      "name_full": "S&P/ASX 200 Materials (Sector)",
+      "name_short": "Mats",
+      "name_abrev": "Materials"
+    },
+    {
+      "index_code": "XMM",
+      "name_full": "S&P/ASX 300 Metals and Mining (Industry)",
+      "name_short": "MTL&MINING",
+      "name_abrev": "Metals and Mining"
+    },
+    {
+      "index_code": "XJR",
+      "name_full": "S&P/ASX 200 RESOURCES",
+      "name_short": "S&P200 Res",
+      "name_abrev": "S&P/ASX 200 Res"
+    }
+  ]
+}
