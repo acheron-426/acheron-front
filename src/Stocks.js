41e7007cdb8e831b344618be0a59f3f21e003714
martypb <martypb+acheron@gmail.com>
2020-12-12 20:53:13 -0800

    minor formatting issues

--- src/Stocks.js
+++ src/Stocks.js
@@ -14,13 +14,88 @@ export const Stocks = () => {
             <h5>Code</h5>
           </td>
           <td>
+            <h5>ISIN code</h5>
+          </td>
+          <td>
+            <h5>Description</h5>
+          </td>
+          <td>
             <h5>Last</h5>
+          </td>
+          <td>
+            <h5>Open</h5>
+          </td>
+          <td>
+            <h5>Day high</h5>
+          </td>
+          <td>
+            <h5>Day low</h5>
+          </td>
+          <td>
+            <h5>Change Price</h5>
+          </td>
+          <td>
+            <h5>Change %</h5>
+          </td>
+          <td>
+            <h5>Volume</h5>
+          </td>
+          <td>
+            <h5>Bid</h5>
+          </td>
+          <td>
+            <h5>Offer</h5>
+          </td>
+          <td>
+            <h5>Prev close</h5>
+          </td>
+          <td>
+            <h5>Prev % chg</h5>
+          </td>
+          <td>
+            <h5>Year high</h5>
+          </td>
+          <td>
+            <h5>Last trade date</h5>
+          </td>
+          <td>
+            <h5>Year high date</h5>
+          </td>
+          <td>
+            <h5>Year low</h5>
+          </td>
+          <td>
+            <h5>Year low date</h5>
+          </td>
+          <td>
+            <h5>Year open</h5>
+          </td>
+          <td>
+            <h5>Year open date</h5>
+          </td>
+          <td>
+            <h5>Year change price</h5>
+          </td>
+          <td>
+            <h5>Year change %</h5>
           </td>
           <td>
             <h5>PE</h5>
           </td>
           <td>
-            <p>avg daily vol.</p>
+            <h5>EPS</h5>
+          </td>
+          <td>
+            <h5>avg daily vol.</h5>
+          </td>
+          <td>
+            <h5>annual dividend yield</h5>
+          </td>
+          <td>
+            <h5>Market Cap</h5>
+          </td>
+          <td>
+            <h5>Number of shares</h5>
           </td>
         </th>
         {stockData.map((data, key) => {
@@ -29,9 +104,34 @@ export const Stocks = () => {
               <Stock
                 key={key}
                 code={data.code}
+                isin_code={data.isin_code}
+                desc_full={data.desc_full}
                 last_price={data.last_price}
+                open_price={data.open_price}
+                day_high_price={data.day_high_price}
+                day_low_price={data.day_low_price}
+                change_price={data.change_price}
+                change_in_percent={data.change_in_percent}
+                volume={data.volume}
+                bid_price={data.bid_price}
+                offer_price={data.offer_price}
+                previous_close_price={data.previous_close_price}
+                previous_day_percentage_change={data.previous_day_percentage_change}
+                year_high_price={data.year_high_price}
+                last_trade_date={data.last_trade_date}
+                year_high_date={data.year_high_date}
+                year_low_price={data.year_low_price}
+                year_low_date={data.year_low_date}
+                year_open_price={data.year_open_price}
+                year_open_date={data.year_open_date}
+                year_change_price={data.year_change_price}
+                year_change_in_percentage={data.year_change_in_percentage}
                 pe={data.pe}
-                average_daily_volume={data.average_daily_volume}
+                eps={data.eps}
+                year_change_in_percentage={data.average_daily_volume}
+                annual_dividend_yield={data.annual_dividend_yield}
+                market_cap={data.market_cap}
+                number_of_shares={data.number_of_shares}
               />
             </div>
           );
@@ -51,22 +151,98 @@ const HomePageHeader = () => {
   );
 };
 
-const Stock = ({ code, last_price, pe, average_daily_volume }) => {
+const Stock = ({ code, isin_code, desc_full, open_price, day_high_price, day_low_price, change_price, change_in_percent, volume, bid_price, offer_price, previous_close_price, previous_day_percentage_change, year_high_price, last_trade_date, year_high_date, year_low_price, year_low_date, year_open_price, year_open_date, year_change_price, year_change_in_percentage, eps, annual_dividend_yield, market_cap, number_of_shares, last_price, pe, average_daily_volume }) => {
   if (!code) return <div />;
   return (
         <tr>
           <td>
-            <h5>{code}</h5>
+            {code}
           </td>
           <td>
-            <h5>{last_price}</h5>
+            {isin_code}
           </td>
           <td>
-            <h5>{pe}</h5>
+            {desc_full}
           </td>
           <td>
-            <p>{average_daily_volume}</p>
+            {last_price}
+          </td>
+          <td>
+            {open_price}
+          </td>
+          <td>
+            {day_high_price}
+          </td>
+          <td>
+            {day_low_price}
+          </td>
+          <td>
+            {change_price}
+          </td>
+          <td>
+            {change_in_percent}
+          </td>
+          <td>
+            {volume}
+          </td>
+          <td>
+            {bid_price}
+          </td>
+          <td>
+            {offer_price}
+          </td>
+          <td>
+            {previous_close_price}
+          </td>
+          <td>
+            {previous_day_percentage_change}
+          </td>
+          <td>
+            {year_high_price}
+          </td>
+          <td>
+            {last_trade_date}
+          </td>
+          <td>
+            {year_high_date}
+          </td>
+          <td>
+            {year_low_price}
+          </td>
+          <td>
+            {year_low_date}
+          </td>
+          <td>
+            {year_open_price}
+          </td>
+          <td>
+            {year_open_date}
+          </td>
+          <td>
+            {year_change_price}
+          </td>
+          <td>
+            {year_change_in_percentage}
+          </td>
+          <td>
+            {pe}
+          </td>
+          <td>
+            {eps}
+          </td>
+          <td>
+            {average_daily_volume}
+          </td>
+          <td>
+            {annual_dividend_yield}
+          </td>
+          <td>
+            {market_cap}
+          </td>
+          <td>
+            {number_of_shares}
           </td>
         </tr>
   );
 };
+
